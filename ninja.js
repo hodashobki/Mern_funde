ed28900bba84ab67f1afd0c8e17a061fa014541e
Hoda Shobaki <shobkihoda@gmail.com>
2021-07-12 00:16:55 +0300

    ninja

---
+++ ninja.js
@@ -0,0 +1,22 @@
+class Ninja{
+    constructor(name,health){
+        this.name=name;
+        this.health=health;
+        this.speed=3;
+        this.strength=3;
+    }
+    sayName(){
+        console.log("This "+this.constructor.name +"'s name is "+ this.name);
+    }
+    showStats(){
+        console.log("The "+this.constructor.name +"'s "+this.name+" has strength of "+this.strength+" and has the speed of "+this.speed);
+    }
+    drinkSake(){
+        this.health+=10;
+        console.log("the health of "+this.constructor.name+" is "+this.health);
+    }
+}
+const ninja=new Ninja("sami",100);
+ninja.sayName();
+ninja.showStats();
+ninja.drinkSake();
